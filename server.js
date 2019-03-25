e5e3f294c69f7bfbfb7d986b3064b29866c09301
Theophile Avoyne <tavoyne@gmail.com>
2019-03-25 11:16:32 +0100

    Added partials header and footer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const homeRouter = require(path.join(__dirname, 'routes/homeRouter'));
 const aboutRouter = require(path.join(__dirname, 'routes/aboutRouter'));
@@ -11,9 +12,11 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.use('/', homeRouter);
 app.use('/about', aboutRouter);
 
 app.listen(port, () => {
-  console.log(`Sever is up on port ${ port }`)
+  console.log(`Sever is up on port ${ port }`);
 });
