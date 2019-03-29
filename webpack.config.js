94f334c656544aa546aa9a2194112be7ba5c72d2
Theophile Avoyne <tavoyne@gmail.com>
2019-03-29 18:59:36 +0100

    Setup dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = (env) => {
     mode: env,
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js',
-      publicPath: '/'
+      publicPath: '/dist/'
     },
     module: {
       rules: [{
