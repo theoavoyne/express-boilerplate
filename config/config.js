376b028180700a8a1ecd328f04fe85dd80be32cb
Theophile Avoyne <tavoyne@gmail.com>
2019-03-30 16:35:01 +0100

    Added production to config.json

--- config/config.js
+++ config/config.js
@@ -1,10 +1,8 @@
 const env = process.env.NODE_ENV;
 
-if (env === 'development') {
-  const config = require('./config.json');
-  const envConfig = config[env];
+const config = require('./config.json');
+const envConfig = config[env];
 
-  Object.keys(envConfig).forEach((key) => {
-    process.env[key] = envConfig[key];
-  });
-};
+Object.keys(envConfig).forEach((key) => {
+  process.env[key] = envConfig[key];
+});
